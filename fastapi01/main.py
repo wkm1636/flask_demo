77e71355c1b7051df4d4eeb5d26d28dcc3dfa069
wkm1636 <45054430+wkm1636@users.noreply.github.com>
2026-07-04 23:02:00 +0800

    hello2

--- fastapi01/main.py
+++ fastapi01/main.py
@@ -16,6 +16,7 @@ def create_user(name: str, age: int):
     return {"name": name, "age": age}
 
 # hello 
+# hello 222
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
